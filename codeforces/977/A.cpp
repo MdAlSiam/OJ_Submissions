44f76c9aa7b539e6813a54f0361c3bf0d0831de9
MdAlSiam <md.al.siam.008@gmail.com>
2018-05-06 20:10:00 +0600

    Add solution for problem `A - Wrong Subtraction` Link: https://codeforces.com/contest/977/problem/A Tags: implementation, *800 Ref: https://codeforces.com/contest/977/submission/37932104

---
+++ codeforces/977/A.cpp
@@ -0,0 +1,40 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <set>
+#include <map>
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+#include <string>
+#include <utility>
+#include <list>
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define dd double
+#define scl(x) scanf("%lld", &x)
+#define scd(x) scanf("%lf", &x)
+#define pi acos(-1.0)
+#define pb push_back
+#define Sort(x) sort(x.begin(), x.end())
+#define For(i, x, y) for(ll i = x; i < y; i++)
+#define pii pair < int, int >
+#define ppi pair < pii, int >
+#define ff first
+#define ss second
+
+int main(){
+	ll n, k;
+	while(scanf("%lld %lld", &n, &k) == 2){
+		for(ll i = 0; i < k; i++){
+			if(n % 10 == 0) n /= 10;
+			else n--;
+		}
+		printf("%lld\n", n);
+	}
+	return 0;
+}
+
+
