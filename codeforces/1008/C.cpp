20e677a49cb67ed4940eab7bbdcc4d11dea34343
MdAlSiam <md.al.siam.008@gmail.com>
2018-07-14 20:33:00 +0600

    Add solution for problem `C - Reorder the Array` Link: https://codeforces.com/contest/1008/problem/C Tags: combinatorics, math, *1300 Ref: https://codeforces.com/contest/1008/submission/40321706

---
+++ codeforces/1008/C.cpp
@@ -0,0 +1,47 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <set>
+#include <map>
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+#include <string>
+#include <utility>
+#include <list>
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define dd double
+#define scl(x) scanf("%lld", &x)
+#define scd(x) scanf("%lf", &x)
+#define pi acos(-1.0)
+#define pb push_back
+#define Sort(x) sort(x.begin(), x.end())
+#define For(i, x, y) for(ll i = x; i < y; i++)
+#define pii pair < int, int >
+#define ppi pair < pii, int >
+#define ff first
+#define ss second
+
+int main(){
+	ll n;
+	while(scl(n) == 1){
+		ll ara[n+1];
+		For(i, 0, n) scl(ara[i]);
+		sort(ara, ara+n);
+		ll pairindex = 0, kount = 0;
+		for(ll i = 1; i < n; i++){
+			//cout << i << " " << pairindex << " " << kount <<  endl;
+			if(ara[i] > ara[pairindex]){
+				pairindex++;
+				kount++;
+			}
+		}
+		printf("%lld\n", kount);
+	}
+	return 0;
+}
+
+
