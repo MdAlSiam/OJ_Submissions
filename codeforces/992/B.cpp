2552939c381baf08a67dde9f15abd250ebfa78dc
MdAlSiam <md.al.siam.008@gmail.com>
2018-09-08 12:50:00 +0600

    Add solution for problem `B - Nastya Studies Informatics` Link: https://codeforces.com/contest/992/problem/B Tags: math, number theory, *1600 Ref: https://codeforces.com/contest/992/submission/42664348

--- codeforces/992/B.cpp
+++ codeforces/992/B.cpp
@@ -1,23 +1,27 @@
 #include <bits/stdc++.h>
 
-using namespace std;
-
 #define ll long long int
 
-ll gcd(ll a, ll b){if (b == 0) return a ; return gcd(b, a % b);}
+using namespace std;
+
+ll gcd(ll a, ll b){
+	if(a % b == 0) return b;
+	return gcd(b, a%b);
+}
 
 int main(){
-	ll l, r, x, y;
+	ll l, r, x, y, k1, k2, ybx;
 	scanf("%lld %lld %lld %lld", &l, &r, &x, &y);
 	if((y % x) != 0){printf("0\n"); return 0;}
-	ll ybx = y / x;
-	ll k1, k2, ans = 0;
-	for(k1 = 1; k1*k1 <= ybx; k1++){
-		if(ybx % k1 != 0) continue;
-		k2 = ybx / k1;
-		if((gcd(k1, k2) == 1) && (l <= k1*x) && (k1*x <= r) && (l <= k2*x) && (k2*x <= r) )
-			if(k1*k1 == ybx) ans++;
-			else ans += 2;
+	ybx = y / x;
+	ll kount = 0;
+	for(ll k1 = 1; k1*k1 <= ybx; k1++){
+		if(ybx % k1 == 0) k2 = ybx / k1;
+		else continue;
+		if((l <= k1*x) && (l <= k2*x) && (k1*x <= r) && (k2*x <= r) && (gcd(k1, k2) == 1)){
+			if(k1*k1 == ybx) kount++;
+			else kount += 2;
+		}
 	}
-	printf("%lld\n", ans);
+	printf("%lld\n", kount);
 }
