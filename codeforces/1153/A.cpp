09af9f98d5915e7a6062b286a304da11acc573bc
MdAlSiam <md.al.siam.008@gmail.com>
2019-04-14 00:27:00 +0600

    Add solution for problem `A - Serval and Bus` Link: https://codeforces.com/contest/1153/problem/A Tags: brute force, math, *1000 Ref: https://codeforces.com/contest/1153/submission/52718473

---
+++ codeforces/1153/A.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long int
+
+int main(){
+    ll n, t;
+    scanf("%lld %lld", &n, &t);
+    ll f[n+10], d[n+10];
+
+    for(ll i = 1; i <= n; i++){
+        scanf("%lld %lld", &f[i], &d[i]);
+    }
+
+    ll ans = LONG_LONG_MAX;
+    ll in = -1;
+
+    for(ll i = 1; i <= n; i++){
+        
+
+        for(ll j = f[i];; j+=d[i]){
+            if(j < ans && j >= t){
+                ans = j;
+                in = i;
+                break;
+            }
+            if(j > ans) break;
+        }
+    }
+
+    printf("%lld\n", in);
+}
