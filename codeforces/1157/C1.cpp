84e296c91104b32f2d3d2dd1569dd704f346fc09
MdAlSiam <md.al.siam.008@gmail.com>
2019-04-26 22:29:00 +0600

    Add solution for problem `C1 - Increasing Subsequence (easy version)` Link: https://codeforces.com/contest/1157/problem/C1 Tags: greedy, *1300 Ref: https://codeforces.com/contest/1157/submission/53363827

--- codeforces/1157/C1.cpp
+++ codeforces/1157/C1.cpp
@@ -1,74 +1,54 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+#define ll long long int
+
 int main(){
-//while(1){
-    int n;
-    scanf("%d", &n);
-
-    int ara[n+4];
-    for(int i = 0; i < n; i++)
-        scanf("%d", ara+i);
-
-    int lp = 0, rp = n-1;
-    int prev = -1;
-    char ans[n+6];
-    int index = 0;
-
-    while(lp <= rp){
-        if(ara[lp] == ara[rp] && ara[lp] > prev){
-            //int index_start = index;
-            int len1(1), len2(1);
-            //ans[index++] = ara[lp];
-
-            for(int k = lp+1; k < n; k++){
-                if(ara[k] > ara[k-1]){len1++;}
-                else break;
+    ll n;
+    while(scanf("%lld", &n) == 1){
+        ll ara[n+5];
+        for(ll i = 0; i < n; i++)
+            scanf("%lld", ara+i);
+        ll lp = 0, rp = n-1;
+        ll prev = -1;
+        ll kount = 0;
+        string ans = "";
+        while(lp <= rp){
+            if(lp == rp && ara[lp] > prev){
+                kount++;
+                ans += "R";
+                break;
             }
-
-            for(int k = rp-1; k >= 0; k--){
-                if(ara[k] > ara[k+1]){len2++;}
-                else break;
+            else if(lp == rp) break;
+            else if(prev < ara[lp] && ara[lp] <= ara[rp]){
+                kount++;
+                ans += "L";
+                prev = ara[lp];
+                lp++;
             }
+            else if(prev < ara[rp] && ara[rp] <= ara[lp]){
+                kount++;
+                ans += "R";
+                prev = ara[rp];
+                rp--;
+            }
+            else if(prev < ara[lp]){
+                kount++;
+                ans += "L";
+                prev = ara[lp];
+                lp++;
+            }
+            else if(prev < ara[rp]){
+                kount++;
+                ans += "R";
+                prev = ara[rp];
+                rp--;
+            }
+            else break;
+        }
 
-
-            for(int ii = 0; ii < len1 && len1 > len2; ii++)
-                ans[index++] =  'L';
-            for(int ii = 0; ii < len2 && len1 <= len2; ii++)
-                ans[index++] =  'R';
-
-            break;
-        }
-        else if(lp == rp && ara[lp] > prev){
-            ans[index++] = 'R';
-            break;
-        }
-        else if(lp == rp) break;
-        else if(prev < ara[lp] && ara[lp] < ara[rp]){
-            ans[index++] = 'L';
-            prev = ara[lp];
-            lp++;
-        }
-        else if(prev < ara[rp] && ara[rp] < ara[lp]){
-            ans[index++] = 'R';
-            prev = ara[rp];
-            rp--;
-        }
-        else if(prev < ara[lp]){
-            ans[index++] = 'L';
-            prev = ara[lp];
-            lp++;
-        }
-        else if(prev < ara[rp]){
-            ans[index++] = 'R';
-            prev = ara[rp];
-            rp--;
-        }
-        else break;
+        printf("%lld\n", kount);
+        cout << ans << endl;
     }
-
-    printf("%d\n", index);
-    for(int i = 0; i < index; i++)
-        printf(i == index-1 ? "%c\n":"%c", ans[i]);
-//}
 }
+
