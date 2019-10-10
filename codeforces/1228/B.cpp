a80afd898268246b989a7e883c146c52db922f7a
MdAlSiam <md.al.siam.008@gmail.com>
2019-10-11 01:06:00 +0600

    Add solution for problem `B - Filling the Grid` Link: https://codeforces.com/contest/1228/problem/B Tags: implementation, math, *1400 Ref: https://codeforces.com/contest/1228/submission/62327465

--- codeforces/1228/B.cpp
+++ codeforces/1228/B.cpp
@@ -4,7 +4,7 @@ using namespace std;
 #define mod 1000000007
 
 int main(){
-
+//while(1){
     int n, m;
     scanf("%d %d", &n, &m);
 
@@ -14,10 +14,22 @@ int main(){
     for(int i = 1; i <= m; i++)
         scanf("%d", c+i);
 
+    if(n == 1 || m == 1){cout << 0 << endl; return 0;}
+
     int kount = 0;
 
     for(int i = 1; i <= n; i++){
         for(int j = 1; j <= m; j++){
+
+            if(i == 1 && c[j] == 0 && r[i] >= j){
+                cout << 0 << endl; return 0;
+            }
+
+            if(j == 1 && r[i] == 0 && c[j] >= i){
+                cout << 0 << endl; return 0;
+            }
+
+
 
             if(r[i]+1 == j && c[j] >= i){
                 cout << 0 << endl; return 0;
@@ -28,11 +40,15 @@ int main(){
 
             if(i == 1 || j == 1) continue;
 
+
+
             bool thr_ro = false;
             bool thr_col = false;
 
             if(j > r[i]+1) thr_ro = true;
             if(i > c[j]+1) thr_col = true;
+
+            //cout << i << " " << j << " => " << thr_ro << " " << thr_col << endl;
 
             if(thr_ro && thr_col) kount++;
 
@@ -46,5 +62,5 @@ int main(){
     }
 
     printf("%d\n", ans);
-
+//}
 }
