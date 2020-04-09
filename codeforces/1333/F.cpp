2a8d27ec588318f26778064c2bb58b2ce9986f9d
MdAlSiam <md.al.siam.008@gmail.com>
2020-04-09 22:46:00 +0600

    Add solution for problem `F - Kate and imperfection` Link: https://codeforces.com/contest/1333/problem/F Tags: greedy, implementation, math, number theory, sortings, two pointers, *2200 Ref: https://codeforces.com/contest/1333/submission/76016256

--- codeforces/1333/F.cpp
+++ codeforces/1333/F.cpp
@@ -1,17 +1,56 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+#define ll long long int
+#define ull unsigned long long int
+#define dd double
+
+#define scl(x) scanf("%lld", &x)
+#define scll(x, y) scanf("%lld %lld", &x, &y)
+#define scd(x) scanf("%lf", &x)
+#define scdd(x, y) scanf("%lf %lf", &x, &y)
+
+#define prl(x) printf("%lld\n", x)
+#define prll(x, y) printf("%lld %lld\n", x, y)
+
+#define For(i, x, y) for(ll i = x; i < y; i++)
+
+#define pb push_back
+#define pll pair <ll, ll >
+#define ff first
+#define ss second
+
+#define maxn 200005 ///2x10^5 // n*log n in 1 sec
+//#define maxn 1000006 ///10^6
+//#define maxn 1000000009 ///10^9
+
+#define pi acos(-1.00)
+#define eps 0.0000000001 ///10^-10
+#define inf LONG_LONG_MAX
+#define mod 1000000007 ///10^9+7
+
+ll t, test, temp;
+ll n, m, k;
+ll a, b, c;
+ll x, y, z;
+
 int main(){
-    int n;
-    cin >> n;
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+//scl(test); while(test--){
+	scl(n);
 
-    int ans[n+1];
-    for(int i = 1; i <= n; i++)
-        for(int j = i+i; j <= n; j += i)
-            ans[j] = i;
+	ll ara[n+4];
+	For(i, 1, n+1){
+		for(ll j = i+i; j <= n; j += i){
+			ara[j] = i;
+		}
+	}
 
-    sort(ans+1, ans+n+1);
+	sort(ara+2, ara+n+1);
 
-    for(int i = 2; i <= n; i++)
-        cout << ans[i] << " ";
-}
+	For(i, 2, n+1) printf("%d ", ara[i]);
+}//}
+///Test(Given, Scratch, Corner data, Code Counter)
