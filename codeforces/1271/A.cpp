d1000ae236049c8b17e647721a1b05472ee510b9
MdAlSiam <md.al.siam.008@gmail.com>
2020-04-10 02:43:00 +0600

    Add solution for problem `A - Suits` Link: https://codeforces.com/contest/1271/problem/A Tags: brute force, greedy, math, *800 Ref: https://codeforces.com/contest/1271/submission/76037463

--- codeforces/1271/A.cpp
+++ codeforces/1271/A.cpp
@@ -1,18 +1,62 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int a, b, c, d, e, f, taken, ans1, ans2;
+#define ll long long int
+#define ull unsigned long long int
+#define dd double
+
+#define scl(x) scanf("%lld", &x)
+#define scll(x, y) scanf("%lld %lld", &x, &y)
+#define scd(x) scanf("%lf", &x)
+#define scdd(x, y) scanf("%lf %lf", &x, &y)
+
+#define prl(x) printf("%lld\n", x)
+#define prll(x, y) printf("%lld %lld\n", x, y)
+
+#define For(i, x, y) for(ll i = x; i < y; i++)
+
+#define pb push_back
+#define pll pair <ll, ll >
+#define ff first
+#define ss second
+
+#define maxn 200005 ///2x10^5 // n*log n in 1 sec
+//#define maxn 1000006 ///10^6
+//#define maxn 1000000009 ///10^9
+
+#define pi acos(-1.00)
+#define eps 0.0000000001 ///10^-10
+#define inf LONG_LONG_MAX
+#define mod 1000000007 ///10^9+7
+
+ll t, test, temp;
+ll n, m, k;
+ll a, b, c, d, e, f;
+ll x, y, z;
 
 int main(){
-    cin >> a >> b >> c >> d >> e >> f;
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+//scl(test); while(test--){
+	scll(a, b);
+	scll(c, d);
+	scll(e, f);
 
-    taken = min(a,d);
-    ans1 = taken*e;
-    ans1 += min(b, min(c, d-taken))*f;
+	ll t2k = min(b, c);
+	t2k = min(t2k, d);
 
-    taken = min(b, min(c, d));
-    ans2 = taken*f;
-    ans2 += min(a, d-taken)*e;
+	ll t1k = min(a, d-t2k);
 
-    cout << max(ans1, ans2) << endl;
-}
+	ll c1 = e*t1k + f*t2k;
+
+	t1k = min(a, d);
+	t2k = min(b, c);
+	t2k = min(t2k, d-t1k);
+
+	ll c2 = e*t1k + f*t2k;
+
+	printf("%lld\n", max(c1, c2));
+}//}
+///Test(Given, Scratch, Corner data, Code Counter)
