98cb34a5837e729c85a67b0aa2ce54dbe4e0df74
MdAlSiam <md.al.siam.008@gmail.com>
2020-04-10 21:20:00 +0600

    Add solution for problem `B - Middle Class` Link: https://codeforces.com/contest/1334/problem/B Tags: greedy, sortings, *1100 Ref: https://codeforces.com/contest/1334/submission/76143632

--- codeforces/1334/B.cpp
+++ codeforces/1334/B.cpp
@@ -1,36 +1,63 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define int long long int
+#define ll long long int
+#define ull unsigned long long int
+#define dd double
 
-int32_t main(){
-    int test;
-    cin >> test;
+#define scl(x) scanf("%lld", &x)
+#define scll(x, y) scanf("%lld %lld", &x, &y)
+#define scd(x) scanf("%lf", &x)
+#define scdd(x, y) scanf("%lf %lf", &x, &y)
 
-    while(test--){
-        int n, x;
-        cin >> n >> x;
-        int ara[n];
-        for(int i = 0; i < n; i++)
-            cin >> ara[i];
+#define prl(x) printf("%lld\n", x)
+#define prll(x, y) printf("%lld %lld\n", x, y)
 
-        sort(ara, ara+n);
+#define For(i, x, y) for(ll i = x; i < y; i++)
+#define Mem(ara, x) memset(ara, x, sizeof(ara))
 
-        int crook = 0, ans = 0;
-        for(int i = n-1; i >= 0; i--){
-            if(ara[i] >= x){
-                ans++;
-                crook += (ara[i] - x);
-            }
-            else{
-                int req = x - ara[i];
-                if(crook >= req){
-                    ans++;
-                    crook -= req;
-                }
-            }
-        }
+#define pb push_back
+#define pll pair <ll, ll >
+#define ff first
+#define ss second
 
-        cout << ans << endl;
-    }
-}
+#define maxn 200005 ///2x10^5 // n*log n in 1 sec
+//#define maxn 1000006 ///10^6
+//#define maxn 1000000009 ///10^9
+
+#define pi acos(-1.00)
+#define eps 0.0000000001 ///10^-10
+#define inf LONG_LONG_MAX
+#define mod 1000000007 ///10^9+7
+
+ll t, test, temp;
+ll n, m, k;
+ll a, b, c;
+ll x, y, z;
+
+int main(){
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+scl(test); while(test--){
+
+	scll(n, k);
+	
+	ll ara[n+4], cs[n+4];
+	For(i, 1, n+1) scl(ara[i]);
+	sort(ara+1, ara+n+1, greater<ll>());
+	cs[1] = ara[1];
+	For(i, 2, n+1) cs[i] = cs[i-1]+ara[i];
+
+	bool found = false;
+	for(ll i = n; i >= 1; i--){
+		if(cs[i]/i >= k){
+			found = true;
+			prl(i);
+			break;
+		}
+	}
+	if(!found) printf("0\n");
+}}
+///Test(Given, Scratch, Corner data, Code Counter)
