259724bd8aef5eca8dc646c764e5ab8c14baf054
MdAlSiam <md.al.siam.008@gmail.com>
2020-04-11 02:21:00 +0600

    Add solution for problem `A - Three Strings` Link: https://codeforces.com/contest/1301/problem/A Tags: implementation, strings, *800 Ref: https://codeforces.com/contest/1301/submission/76212158

--- codeforces/1301/A.cpp
+++ codeforces/1301/A.cpp
@@ -1,23 +1,58 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int test;
-string a, b, c;
-int len;
-bool yes;
+#define ll long long int
+#define ull unsigned long long int
+#define dd double
+
+#define scl(x) scanf("%lld", &x)
+#define scll(x, y) scanf("%lld %lld", &x, &y)
+#define scd(x) scanf("%lf", &x)
+#define scdd(x, y) scanf("%lf %lf", &x, &y)
+
+#define prl(x) printf("%lld\n", x)
+#define prll(x, y) printf("%lld %lld\n", x, y)
+
+#define For(i, x, y) for(ll i = x; i < y; i++)
+#define Mem(ara, x) memset(ara, x, sizeof(ara))
+
+#define pb push_back
+#define pll pair <ll, ll >
+#define ff first
+#define ss second
+
+#define maxn 200005 ///2x10^5 // n*log n in 1 sec
+//#define maxn 1000006 ///10^6
+//#define maxn 1000000009 ///10^9
+
+#define pi acos(-1.00)
+#define eps 0.0000000001 ///10^-10
+#define inf LONG_LONG_MAX
+#define mod 1000000007 ///10^9+7
+
+ll t, test, temp;
+ll n, m, k;
+//ll a, b, c;
+ll x, y, z;
 
 int main(){
-    cin >> test;
-    while(test--){
-        cin >> a >> b >> c;
-        len = a.length();
-        yes = true;
-        for(int i = 0; i < len; i++){
-            if(a[i] == c[i] or b[i] == c[i]);
-            else yes = false;
-        }
-        if(yes) printf("Yes\n");
-        else printf("No\n");
-    }
-}
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+scl(test); while(test--){
+	string a, b, c;
+	cin >> a >> b >> c;
+	n = a.length();
 
+	bool yes = true;
+
+	For(i, 0, n){
+		if(a[i] == c[i] || b[i] == c[i]){}
+		else yes = false;
+	}
+
+	if(yes) printf("YES\n");
+	else printf("NO\n");
+}}
+///Test(Given, Scratch, Corner data, Code Counter)
