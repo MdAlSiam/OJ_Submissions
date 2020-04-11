bfc482f1e0c0b0e01a7aa0c0ed86ae4ada4b0f01
MdAlSiam <md.al.siam.008@gmail.com>
2020-04-11 23:56:00 +0600

    Add solution for problem `B - Rebranding` Link: https://codeforces.com/contest/591/problem/B Tags: implementation, strings, *1200 Ref: https://codeforces.com/contest/591/submission/76254184

--- codeforces/591/B.cpp
+++ codeforces/591/B.cpp
@@ -1,28 +1,37 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n, m;
-int curr_val[26];
-char str[200005];
-char ch1, ch2;
-
 int main(){
-    scanf("%d %d", &n, &m);
-    scanf(" %s", &str);
+  
+	#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+	#endif
+	
 
-    for(int i = 0; i < 26; i++)
-        curr_val[i] = i;
+	int n, m;
+	scanf("%d %d", &n, &m);
 
-    for(int i = 0; i < m; i++){
-        scanf(" %c %c", &ch1, &ch2);
-        for(int j = 0; j < 26; j++){
-            if(curr_val[j] == ch1 - 'a')
-                curr_val[j] = ch2 - 'a';
-            else if(curr_val[j] == ch2 - 'a')
-                curr_val[j] = ch1 - 'a';
-        }
-    }
-    for(int i = 0; i < n; i++)
-        printf("%c", curr_val[str[i]-'a']+'a');
-    printf("\n");
+	char str[n+5];
+	scanf(" %s", &str);
+
+	int state[26];
+	for(int i = 0; i < 26; i++)
+		state[i] = i;
+
+	while(m--){
+		char x, y;
+		scanf(" %c %c", &x, &y);
+		for(int i = 0; i < 26; i++){
+			if(state[i] == x - 'a')
+				state[i] = y - 'a';
+			else if(state[i] == y - 'a')
+				state[i] = x - 'a';
+		}
+	}
+
+	for(int i = 0; i < n; i++)
+		str[i] = state[str[i]-'a'] + 'a';
+
+	printf("%s\n", str);
 }
