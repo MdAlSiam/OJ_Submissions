73c4a3bb5742b865eac8b04c74710145e9ee9cc9
MdAlSiam <md.al.siam.008@gmail.com>
2020-06-18 21:12:00 +0600

    Add solution for problem `A - C+=` Link: https://codeforces.com/contest/1368/problem/A Tags: brute force, greedy, implementation, math, *800 Ref: https://codeforces.com/contest/1368/submission/84195604

--- codeforces/1368/A.cpp
+++ codeforces/1368/A.cpp
@@ -1,32 +1,70 @@
-#include <bits/stdc++.h>
 using namespace std;
+#include <bits/stdc++.h>
+
+#define ll long long int
+#define ull unsigned long long int
+#define dd double
+
+#define scl(x) scanf("%lld", &x)
+#define scll(x, y) scanf("%lld %lld", &x, &y)
+#define scd(x) scanf("%lf", &x)
+#define scdd(x, y) scanf("%lf %lf", &x, &y)
+
+#define prl(x) printf("%lld\n", x)
+#define prll(x, y) printf("%lld %lld\n", x, y)
+
+#define For(i, x, y) for(ll i = x; i < y; i++)
+#define Mem(ara, x) memset(ara, x, sizeof(ara))
+
+#define pb push_back
+#define pll pair <ll, ll >
+#define ff first
+#define ss second
+
+#define maxn 200005 ///2x10^5 // n*log n in 1 sec
+//#define maxn 1000006 ///10^6
+//#define maxn 1000000009 ///10^9
+
+#define pi acos(-1.00)
+#define eps 0.0000000001 ///10^-10
+#define inf LONG_LONG_MAX
+#define mod 1000000007 ///10^9+7
+
+/*******************************************************************************************/
+
+ll t, test, temp;
+ll n, m, k, kount;
+ll a, b, c, ans;
+ll x, y, z;
+
+/*_________________________________________________________________________________________*/
 
 int main(){
+while(scl(test) == 1){
+while(test--){
+    scll(a, b);
+    scl(n);
 
-    int test;
-    cin >> test;
+    if(a <= b);
+    else swap(a, b);
 
-    while(test--){
+    kount = 0;
 
-        int a, b, n;
-        cin >> a >> b >> n;
+    while(1){
+        a += b;
+        kount++;
+        if(a > n) break;
 
-        if(a <= b);
-        else swap(a, b);
-
-        int kount = 0;
-
-        while(1){
-            a += b;
-            kount++;
-            if(a > n) break;
-
-            b += a;
-            kount++;
-            if(b > n) break;
-        }
-
-        cout << kount << endl;
-
+        b += a;
+        kount++;
+        if(b > n) break;
     }
+
+    prl(kount);
 }
+}}
+
+/*******************************************************************************************/
+
+///OK ok
+
