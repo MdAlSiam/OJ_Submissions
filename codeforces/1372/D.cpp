9b0bed85dcbd3773022a0cbc10cec851460229bc
MdAlSiam <md.al.siam.008@gmail.com>
2020-07-13 17:40:00 +0600

    Add solution for problem `D - Omkar and Circle` Link: https://codeforces.com/contest/1372/problem/D Tags: brute force, dp, games, greedy, *2100 Ref: https://codeforces.com/contest/1372/submission/86743434

--- codeforces/1372/D.cpp
+++ codeforces/1372/D.cpp
@@ -4,10 +4,11 @@ using namespace std;
 const int64_t maxn = 200005;
 int64_t n;
 int64_t ara[maxn];
-int64_t startfrom[maxn+maxn];
+int64_t oddstart[maxn];
+int64_t evenstart[maxn];
 int64_t ans = -1;
 int64_t slide_size;
-int64_t index1 = 1;
+int64_t index1 = 1, index2 = 1;
 
 int32_t main(){
     cin >> n;
@@ -17,21 +18,27 @@ int32_t main(){
     slide_size = (n / 2) + 1;
 
     for(int64_t i = 1; i <= n; i += 2){
-        startfrom[index1] = ara[i] + startfrom[index1-1];
+        oddstart[index1] = ara[i] + oddstart[index1-1];
         index1++;
     }
     for(int64_t i = 2; i <= n; i += 2){
-        startfrom[index1] = ara[i] + startfrom[index1-1];
+        oddstart[index1] = ara[i] + oddstart[index1-1];
         index1++;
+    }
+
+    for(int64_t i = 2; i <= n; i += 2){
+        evenstart[index2] = ara[i] + evenstart[index2-1];
+        index2++;
     }
     for(int64_t i = 1; i <= n; i += 2){
-        startfrom[index1] = ara[i] + startfrom[index1-1];
-        index1++;
+        evenstart[index2] = ara[i] + evenstart[index2-1];
+        index2++;
     }
 
-
-    for(int64_t i = 1; i <= n; i++)
-        ans = max(ans, startfrom[i+slide_size-1] - startfrom[i-1]);
+    for(int64_t i = 1; i <= slide_size; i++)
+        ans = max(ans, oddstart[i+slide_size-1] - oddstart[i-1]);
+    for(int64_t i = 1; i <= slide_size; i++)
+        ans = max(ans, evenstart[i+slide_size-1] - evenstart[i-1]);
 
     cout << ans << endl;
 }
