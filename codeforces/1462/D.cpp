050c272714f730119fac0be34ed8d17bc9c6dcbd
MdAlSiam <md.al.siam.008@gmail.com>
2020-12-15 21:54:00 +0600

    Add solution for problem `D - Add to Neighbour and Remove` Link: https://codeforces.com/contest/1462/problem/D Tags: greedy, math, number theory, *1400 Ref: https://codeforces.com/contest/1462/submission/101322029

--- codeforces/1462/D.cpp
+++ codeforces/1462/D.cpp
@@ -1,44 +1,120 @@
-#include <bits/stdc++.h>
+/// In the name of Allah SWT
+
 using namespace std;
+#include <bits/stdc++.h>
 
-int main() {
-    int test;
-    cin >> test;
-    while (test--) {
-        int n;
-        cin >> n;
-        map<int, int> csPos;
-        csPos.clear();
-        int sum = 0;
-        for (int i = 0; i < n; i++) {
-            int temp;
-            cin >> temp;
-            sum += temp;
-            csPos[sum] = i;
-        }
+#define ll long long int
+#define dd double
+#define scl(x) scanf("%lld", &x)
+#define scll(x, y) scanf("%lld %lld", &x, &y)
+#define scd(x) scanf("%lf", &x)
+#define scdd(x, y) scanf("%lf %lf", &x, &y)
 
-        int minOps = 100000;
+#define prl(x) printf("%lld\n", x)
+#define prll(x, y) printf("%lld %lld\n", x, y)
+#define prYes printf("YES\n")
+#define prNo printf("NO\n")
+#define aNewLine printf("\n")
 
-        for (int i = 1; i <= sum; i++) {
-            int opsHere = 0;
-            int lastCountered = -1;
-            int minOpsHere = 100000;
+#define ON(n, i) (n|(1LL<<i))
+#define OFF(n, i) (n&(~(1LL<<i)))
+#define CHK(n, i) (n&(1LL<<i))
 
-            for (int j = 1; i*j <= sum; j++) {
-                if (csPos.count(i*j)) {
-                    opsHere += csPos[i*j] - lastCountered - 1;
-                    lastCountered = csPos[i*j];
-                    if (i*j == sum) {
-                        minOpsHere = opsHere;
-                        break;
-                    }
-                }
-                else break;
+#define For(i, x, y) for(ll i = x; i < y; i++)
+#define Mem(ara, x) memset(ara, x, sizeof(ara))
+
+#define pb push_back
+#define pll pair<ll, ll >
+#define ff first
+#define ss second
+
+#define maxn 200005 ///2x10^5 + 5
+//#define maxn 1000006 ///10^6 + 6
+//#define maxn 1000000009 ///10^9 + 9
+
+#define pi acos(-1.00)
+#define eps 0.0000000001 ///10^-10
+#define inf LONG_LONG_MAX
+#define mod 1000000007 ///10^9+7
+
+ll t, test, temp;
+ll n, m, k, kount;
+ll a, b, c, ans, u, v;
+ll x, y, z = -1, maxi, mini;
+map <ll, ll> seenAt;
+ll lastCovered;
+
+void solve() {
+    scl(n);
+    ll ara[n+10];
+    ara[0] = 0;
+    For (i, 1, n+1) scl(ara[i]);
+    ll cs[n+10];
+    cs[0] = 0;
+    For (i, 1, n+1) cs[i] = ara[i] + cs[i-1];
+    ll sum = 0;
+    For (i, 1, n+1) sum += ara[i];
+    seenAt.clear();
+    For (i, 1, n+1) seenAt[cs[i]] = i;
+
+    ans = inf;
+
+    For (target, 1, sum+1) {
+        /*
+        seenAt.clear();
+        lastCovered = 0;
+        seenAt[0] = 0;
+        bool gotHere = true;
+        ll ansHere = 0;
+        For (i, 1, n+1) {
+            ll toGet = cs[i]-target;
+            if (toGet < 0) {
+                continue;
+            }
+            if (not seenAt.count(toGet)) {
+                continue;
             }
 
-            minOps = min(minOps, minOpsHere);
+            if (seenAt[toGet] != lastCovered) {
+                gotHere = false;
+                break;
+            }
+            else {
+                ansHere += (i-lastCovered-1);
+                lastCovered = i;
+            }
+
+            seenAt[cs[i]] = i;
         }
 
-        cout << minOps << endl;
+        if (lastCovered == n) {
+            ans = min(ans, ansHere);
+        }
+        */
+        ll targetGoesTo = target;
+        ll lastCovered = 0;
+        ll opHere = 0;
+        while (true) {
+            if (seenAt.count(targetGoesTo)) {
+                opHere += seenAt[targetGoesTo] - lastCovered - 1;
+                lastCovered = seenAt[targetGoesTo];
+                targetGoesTo += target;
+            }
+            else {
+                break;
+            }
+        }
+        if (lastCovered == n) {
+            ans = min(ans, opHere);
+        }
     }
+
+    cout << ans << endl;
+
+}
+
+int main() {
+    test = 1;
+    cin >> test;
+    while (test--) solve();
 }
