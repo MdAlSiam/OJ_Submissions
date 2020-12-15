ff07bc424ed687ba4707953e797bb0fc08e1b721
MdAlSiam <md.al.siam.008@gmail.com>
2020-12-15 20:39:00 +0600

    Add solution for problem `A - Favorite Sequence` Link: https://codeforces.com/contest/1462/problem/A Tags: implementation, two pointers, *800 Ref: https://codeforces.com/contest/1462/submission/101261861

--- codeforces/1462/A.cpp
+++ codeforces/1462/A.cpp
@@ -5,58 +5,56 @@ using namespace std;
 
 #define ll long long int
 #define dd double
+#define scl(x) scanf("%lld", &x)
+#define scll(x, y) scanf("%lld %lld", &x, &y)
+#define scd(x) scanf("%lf", &x)
+#define scdd(x, y) scanf("%lf %lf", &x, &y)
 
-#define scl(x)              scanf("%lld", &x)
-#define scll(x, y)          scanf("%lld %lld", &x, &y)
-#define scd(x)              scanf("%lf", &x)
-#define scdd(x, y)          scanf("%lf %lf", &x, &y)
+#define prl(x) printf("%lld\n", x)
+#define prll(x, y) printf("%lld %lld\n", x, y)
+#define prYes printf("YES\n")
+#define prNo printf("NO\n")
+#define aNewLine printf("\n")
 
-#define prl(x)              printf("%lld\n", x)
-#define prll(x, y)          printf("%lld %lld\n", x, y)
-#define prYes               printf("YES\n")
-#define prNo                printf("NO\n")
-#define aNewLine            printf("\n")
+#define ON(n, i) (n|(1LL<<i))
+#define OFF(n, i) (n&(~(1LL<<i)))
+#define CHK(n, i) (n&(1LL<<i))
 
-#define ON(n, i)            (n|(1LL<<i))
-#define OFF(n, i)           (n&(~(1LL<<i)))
-#define CHK(n, i)           (n&(1LL<<i))
+#define For(i, x, y) for(ll i = x; i < y; i++)
+#define Mem(ara, x) memset(ara, x, sizeof(ara))
 
-#define For(i, x, y)        for(ll i = x; i < y; i++)
-#define Mem(ara, x)         memset(ara, x, sizeof(ara))
+#define pb push_back
+#define pll pair<ll, ll >
+#define ff first
+#define ss second
 
-#define pb                  push_back
-#define pll                 pair<ll, ll >
-#define ff                  first
-#define ss                  second
+#define maxn 200005 ///2x10^5 + 5
+//#define maxn 1000006 ///10^6 + 6
+//#define maxn 1000000009 ///10^9 + 9
 
-#define maxn                200005LL ///2x10^5 + 5
-
-#define pi                  acos(-1.00)
-#define eps                 0.0000000001 ///10^-10
-#define inf                 LONG_LONG_MAX
-#define mod                 1000000007LL ///10^9+7
+#define pi acos(-1.00)
+#define eps 0.0000000001 ///10^-10
+#define inf LONG_LONG_MAX
+#define mod 1000000007 ///10^9+7
 
 ll t, test, temp;
 ll n, m, k, kount;
 ll a, b, c, ans, u, v;
 ll x, y, z = -1, maxi, mini;
 
-ll ara[maxn];
-string str;
-
 void solve() {
     scl(n);
     ll ara[n];
     For (i, 0, n) scl(ara[i]);
-
     ll lo = 0, hi = n-1;
 
     while (lo <= hi) {
         if (lo == hi) {
             printf("%lld ", ara[lo]);
-            break;
         }
-        printf("%lld %lld ", ara[lo], ara[hi]);
+        else {
+            printf("%lld %lld ", ara[lo], ara[hi]);
+        }
         lo++;
         hi--;
     }
@@ -65,6 +63,6 @@ void solve() {
 
 int main() {
     test = 1;
-    scl(test);
+    cin >> test;
     while (test--) solve();
 }
