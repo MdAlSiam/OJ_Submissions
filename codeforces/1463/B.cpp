8a7021162f01c8153e52dbb1c980485b59f31422
MdAlSiam <md.al.siam.008@gmail.com>
2020-12-17 23:26:00 +0600

    Add solution for problem `B - Find The Array` Link: https://codeforces.com/contest/1463/problem/B Tags: bitmasks, constructive algorithms, greedy, *1400 Ref: https://codeforces.com/contest/1463/submission/101584472

--- codeforces/1463/B.cpp
+++ codeforces/1463/B.cpp
@@ -5,7 +5,6 @@ using namespace std;
 
 #define ll long long int
 #define dd double
-
 #define scl(x) scanf("%lld", &x)
 #define scll(x, y) scanf("%lld %lld", &x, &y)
 #define scd(x) scanf("%lf", &x)
@@ -29,14 +28,14 @@ using namespace std;
 #define ff first
 #define ss second
 
-#define maxn 200005LL ///2x10^5 + 5
-//#define maxn 1000006LL ///10^6 + 6
-//#define maxn 1000000009LL ///10^9 + 9
+#define maxn 200005 ///2x10^5 + 5
+//#define maxn 1000006 ///10^6 + 6
+//#define maxn 1000000009 ///10^9 + 9
 
 #define pi acos(-1.00)
 #define eps 0.0000000001 ///10^-10
 #define inf LONG_LONG_MAX
-#define mod 1000000007LL ///10^9+7
+#define mod 1000000007 ///10^9+7
 
 ll t, test, temp;
 ll n, m, k, kount;
@@ -45,24 +44,24 @@ ll x, y, z = -1, maxi, mini;
 
 void solve() {
     scl(n);
-    ll ara[n];
-    For (i, 0, n) scl(ara[i]);
-    ll oddIndexElementSum = 0, evenIndexElementSum = 0;
-    For (i, 0, n) {
-        if (i&1) oddIndexElementSum += ara[i];
+    ll ara[n+1];
+    ll oddIndexElementsSum = 0, evenIndexElementSum = 0;
+    For (i, 1, n+1) {
+        scl(ara[i]);
+        if (i&1) oddIndexElementsSum += ara[i];
         else evenIndexElementSum += ara[i];
     }
 
-    if (oddIndexElementSum > evenIndexElementSum) {
-        For (i, 0, n) {
+    if (oddIndexElementsSum > evenIndexElementSum) {
+        For (i, 1, n+1) {
             if (i&1) printf("%lld ", ara[i]);
             else printf("1 ");
         }
         aNewLine;
     }
     else {
-        For (i, 0, n) {
-            if (!(i&1)) printf("%lld ", ara[i]);
+        For (i, 1, n+1) {
+            if (not(i&1)) printf("%lld ", ara[i]);
             else printf("1 ");
         }
         aNewLine;
