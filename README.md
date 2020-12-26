11fe83527e83c7b56fafb32b667baea3146d54ec
MdAlSiam <md.al.siam.008@gmail.com>
2020-12-26 11:31:00 +0600

    Add solution for problem `B - Find the Spruce` Link: https://codeforces.com/contest/1461/problem/B Tags: brute force, dp, implementation, *1400 Ref: https://codeforces.com/contest/1461/submission/102367061

--- README.md
+++ README.md
@@ -186,15 +186,15 @@ A repository to keep track of problem solving practice, containing solutions fro
 629 | [E1 - Close Tuples (easy version)](https://codeforces.com/contest/1462/problem/E1) | [GNU C++17 (64)](./codeforces/1462/E1.cpp) | `binary search` `combinatorics` `math` `sortings` `two pointers` `*1500` | Dec/28/2020 22:14 | 
 628 | [D - Divide and Summarize](https://codeforces.com/contest/1461/problem/D) | [GNU C++17 (64)](./codeforces/1461/D.cpp) | `binary search` `brute force` `data structures` `divide and conquer` `implementation` `sortings` `*1600` | Dec/27/2020 13:41 | 
 627 | [C - Random Events](https://codeforces.com/contest/1461/problem/C) | [GNU C++17 (64)](./codeforces/1461/C.cpp) | `dp` `math` `probabilities` `*1500` | Dec/27/2020 00:56 | 
-626 | [A - String Generation](https://codeforces.com/contest/1461/problem/A) | [GNU C++17 (64)](./codeforces/1461/A.cpp) | `constructive algorithms` `greedy` `*800` | Dec/26/2020 10:05 | 
-625 | [C - Berpizza](https://codeforces.com/contest/1468/problem/C) | [GNU C++17 (64)](./codeforces/1468/C.cpp) | `data structures` `implementation` `*1400` | Dec/26/2020 01:32 | 
-624 | [K - The Robot](https://codeforces.com/contest/1468/problem/K) | [GNU C++17 (64)](./codeforces/1468/K.cpp) | `brute force` `implementation` `*1600` | Dec/25/2020 23:35 | 
-623 | [N - Waste Sorting](https://codeforces.com/contest/1468/problem/N) | [GNU C++17 (64)](./codeforces/1468/N.cpp) | `greedy` `implementation` `*900` | Dec/25/2020 17:56 | 
-622 | [E - Four Segments](https://codeforces.com/contest/1468/problem/E) | [GNU C++17 (64)](./codeforces/1468/E.cpp) | `greedy` `*800` | Dec/25/2020 17:42 | 
-621 | [C - Row GCD](https://codeforces.com/contest/1459/problem/C) | [GNU C++17 (64)](./codeforces/1459/C.cpp) | `math` `number theory` `*1600` | Dec/19/2020 22:34 | 
-620 | [B - Move and Turn](https://codeforces.com/contest/1459/problem/B) | [GNU C++17 (64)](./codeforces/1459/B.cpp) | `dp` `math` `*1300` | Dec/19/2020 20:56 | 
-619 | [C - Plant](https://codeforces.com/contest/186/problem/C) | [GNU C++17 (64)](./codeforces/186/C.cpp) | `dp` `math` `matrices` `number theory` `*1300` | Dec/19/2020 02:05 | 
-618 | [B - Find the Spruce](https://codeforces.com/contest/1461/problem/B) | [GNU C++17 (64)](./codeforces/1461/B.cpp) | `brute force` `dp` `implementation` `*1400` | Dec/13/2020 00:56 | 
+626 | [B - Find the Spruce](https://codeforces.com/contest/1461/problem/B) | [GNU C++17 (64)](./codeforces/1461/B.cpp) | `brute force` `dp` `implementation` `*1400` | Dec/26/2020 11:31 | 
+625 | [A - String Generation](https://codeforces.com/contest/1461/problem/A) | [GNU C++17 (64)](./codeforces/1461/A.cpp) | `constructive algorithms` `greedy` `*800` | Dec/26/2020 10:05 | 
+624 | [C - Berpizza](https://codeforces.com/contest/1468/problem/C) | [GNU C++17 (64)](./codeforces/1468/C.cpp) | `data structures` `implementation` `*1400` | Dec/26/2020 01:32 | 
+623 | [K - The Robot](https://codeforces.com/contest/1468/problem/K) | [GNU C++17 (64)](./codeforces/1468/K.cpp) | `brute force` `implementation` `*1600` | Dec/25/2020 23:35 | 
+622 | [N - Waste Sorting](https://codeforces.com/contest/1468/problem/N) | [GNU C++17 (64)](./codeforces/1468/N.cpp) | `greedy` `implementation` `*900` | Dec/25/2020 17:56 | 
+621 | [E - Four Segments](https://codeforces.com/contest/1468/problem/E) | [GNU C++17 (64)](./codeforces/1468/E.cpp) | `greedy` `*800` | Dec/25/2020 17:42 | 
+620 | [C - Row GCD](https://codeforces.com/contest/1459/problem/C) | [GNU C++17 (64)](./codeforces/1459/C.cpp) | `math` `number theory` `*1600` | Dec/19/2020 22:34 | 
+619 | [B - Move and Turn](https://codeforces.com/contest/1459/problem/B) | [GNU C++17 (64)](./codeforces/1459/B.cpp) | `dp` `math` `*1300` | Dec/19/2020 20:56 | 
+618 | [C - Plant](https://codeforces.com/contest/186/problem/C) | [GNU C++17 (64)](./codeforces/186/C.cpp) | `dp` `math` `matrices` `number theory` `*1300` | Dec/19/2020 02:05 | 
 617 | [B - Balls of Steel](https://codeforces.com/contest/1450/problem/B) | [GNU C++17 (64)](./codeforces/1450/B.cpp) | `brute force` `geometry` `greedy` `*1000` | Dec/06/2020 21:30 | 
 616 | [A - Avoid Trygub](https://codeforces.com/contest/1450/problem/A) | [GNU C++17 (64)](./codeforces/1450/A.cpp) | `constructive algorithms` `sortings` `*800` | Dec/06/2020 21:01 | 
 615 | [B - Many 110](https://atcoder.jp/contests/arc110/tasks/arc110_b) | [C++ (GCC 9.2.1)](./atcoder/arc110/B.cpp) | `AtCoder` `*400` | Dec/05/2020 19:49 | 
--- codeforces/1461/B.cpp
+++ codeforces/1461/B.cpp
@@ -1,98 +1,100 @@
-#include <bits/stdc++.h>
+/// In the name of Allah SWT
+
 using namespace std;
+#include <bits/stdc++.h>
 
-int test;
-int n, m;
-string str[505];
-int cumGrid[505][505];
-int ans;
-int currRo, currColBegin, currColEnd;
-int astersikKount;
-int roSegmentLen;
+#define ll long long int
+#define dd double
 
-int main() {
-    cin >> test;
+#define scl(x) scanf("%lld", &x)
+#define scll(x, y) scanf("%lld %lld", &x, &y)
+#define scd(x) scanf("%lf", &x)
+#define scdd(x, y) scanf("%lf %lf", &x, &y)
 
-    while (test--) {
-        cin >> n >> m;
+#define prl(x) printf("%lld\n", x)
+#define prll(x, y) printf("%lld %lld\n", x, y)
+#define prYes printf("YES\n")
+#define prNo printf("NO\n")
+#define aNewLine printf("\n")
 
-        for (int i = 0; i < n; i++) {
-            cin >> str[i];
-        }
+#define ON(n, i) (n|(1LL<<i))
+#define OFF(n, i) (n&(~(1LL<<i)))
+#define CHK(n, i) (n&(1LL<<i))
 
-        //cout << "Got here" << endl;
+#define For(i, x, y) for(ll i = x; i < y; i++)
+#define Mem(ara, x) memset(ara, x, sizeof(ara))
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                cumGrid[i][j] = 0;
+#define pb push_back
+#define pll pair<ll, ll >
+#define ff first
+#define ss second
+
+#define maxn 200005LL ///2x10^5 + 5
+//#define maxn 1000006LL ///10^6 + 6
+//#define maxn 1000000009LL ///10^9 + 9
+
+#define pi acos(-1.00)
+#define eps 0.0000000001 ///10^-10
+#define inf LONG_LONG_MAX
+#define mod 1000000007LL ///10^9+7
+
+ll t, test, temp;
+ll n, m, k, kount;
+ll a, b, c, ans, u, v;
+ll x, y, z = -1, maxi, mini;
+
+void solve() {
+    scll(n, m);
+    char mat[n+1][m+1];
+    For (i, 0, n) scanf(" %s", mat[i]);
+    
+    ll csgrid[n+1][m+1];
+    For (i, 0, n) {
+        For (j, 0, m) {
+            if (j == 0) {
+                if (mat[i][j] == '*') csgrid[i][j] = 1;
+                else csgrid[i][j] = 0;
+            }
+            else {
+                if (mat[i][j] == '*') csgrid[i][j] = csgrid[i][j-1]+1;
+                else csgrid[i][j] = csgrid[i][j-1];
             }
         }
+    }
+    
+    ans = 0;
 
-        //cout << "Got here 2" << endl;
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                //cumGrid[i][j] = 0;
-                
-                if (str[i][j] == '*') {
-                    if (j == 0) {
-                        cumGrid[i][j] = 1;
+    For (i, 0, n) {
+        For (j, 0, m) {
+            if (mat[i][j] == '*') {
+                ans++;
+                ll ro = i;
+                ll colBegin = j, colEnd = j;
+                while (true) {
+                    ro++;
+                    colBegin--;
+                    colEnd++;
+                    if (0 <= ro and ro < n and 0 <= colBegin and colEnd < m) {
+                        // Don't care your feelings
+                        ll totalPlaces = colEnd-colBegin+1;
+                        ll totalAsterisks = csgrid[ro][colEnd]-csgrid[ro][colBegin];
+                        if (mat[ro][colBegin] == '*') totalAsterisks++;
+                        if (totalAsterisks == totalPlaces) ans++;
+                        else break;
                     }
                     else {
-                        cumGrid[i][j] += cumGrid[i][j-1] + 1;
+                        break;
                     }
                 }
             }
         }
-
-        //cout << "Got here 3" << endl;
-
-        for (int i = 0; false and i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                cout << cumGrid[i][j] << " ";
-            }
-            cout << endl;
-        }
-
-        ans = 0;
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (str[i][j] == '*') {
-                    ans++;
-                    currRo = i;
-                    currColBegin = j;
-                    currColEnd = j;
-                    while (true) {
-                        currRo++;
-                        currColBegin--;
-                        currColEnd++;
-
-                        if (0 <= currRo and currRo < n \
-                        and 0 <= currColBegin and currColEnd < m) {
-
-                        }
-                        else {
-                            break;
-                        }
-
-                        astersikKount = \
-                        cumGrid[currRo][currColEnd] - cumGrid[currRo][currColBegin];
-                        if (cumGrid[currRo][currColBegin] > 0) astersikKount++; //?
-
-                        roSegmentLen = currColEnd - currColBegin + 1;
-
-                        if (astersikKount == roSegmentLen) {
-                            ans++;
-                        }
-                        else {
-                            break;
-                        }
-                    }
-                }
-            }
-        }
-
-        cout << ans << endl;
     }
-} 
+
+    prl(ans);
+}
+
+int main() {
+    test = 1;
+    scl(test);
+    while (test--) solve();
+}
