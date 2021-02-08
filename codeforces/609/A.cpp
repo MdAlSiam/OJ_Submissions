b6266fd001c274584a75cf541da5b939cd696bb8
MdAlSiam <md.al.siam.008@gmail.com>
2021-02-09 01:21:00 +0600

    Add solution for problem `A - Флеш-карты` Link: https://codeforces.com/contest/609/problem/A Tags: greedy, implementation, sortings, *800 Ref: https://codeforces.com/contest/609/submission/106935488

---
+++ codeforces/609/A.cpp
@@ -0,0 +1,95 @@
+/// In the name of Allah SWT
+
+using namespace std;
+#include <bits/stdc++.h>
+
+#define ll long long int
+#define dd double
+
+#define scl(x) scanf("%lld", &x)
+#define scll(x, y) scanf("%lld %lld", &x, &y)
+#define scd(x) scanf("%lf", &x)
+#define scdd(x, y) scanf("%lf %lf", &x, &y)
+
+#define prl(x) printf("%lld\n", x)
+#define prll(x, y) printf("%lld %lld\n", x, y)
+#define prYes printf("YES\n")
+#define prNo printf("NO\n")
+#define aNewLine printf("\n")
+
+#define ON(n, i) (n|(1LL<<i))
+#define OFF(n, i) (n&(~(1LL<<i)))
+#define CHK(n, i) (n&(1LL<<i))
+
+#define For(i, x, y) for(ll i = x; i < y; i++)
+#define Mem(ara, x) memset(ara, x, sizeof(ara))
+
+#define pb push_back
+#define pll pair<ll, ll >
+#define ff first
+#define ss second
+
+#define maxn 200005LL ///2x10^5 + 5
+//#define maxn 1000006LL ///10^6 + 6
+//#define maxn 1000000009LL ///10^9 + 9
+
+#define pi acos(-1.00)
+#define eps 0.0000000001 ///10^-10
+#define inf LONG_LONG_MAX
+#define mod 1000000007LL ///10^9+7
+
+ll t, test, temp;
+ll n, m, k, kount;
+ll a, b, c, ans, u, v;
+ll x, y, z = -1, maxi, mini;
+
+ll ara[maxn];
+string str;
+
+void solve() {
+    scl(n);
+    scl(m);
+    ll ara[n], taken[n];
+
+    For (i, 0, n) {
+        scl(ara[i]);
+    }
+
+    For (i, 0, n) {
+        taken[i] = 0;
+    }
+
+    ll allocated = 0;
+
+    ll used = 0;
+
+    while (true) {
+        ll biggest = -1;
+        ll indexToBiggest = -1;
+
+        For (i, 0, n) {
+            if (taken[i] == 0) {
+                if (ara[i] > biggest) {
+                    biggest = ara[i];
+                    indexToBiggest = i;
+                }
+            }
+        }
+
+        allocated += biggest;
+        taken[indexToBiggest] = 1;
+        used++;
+
+        if (allocated >= m) {
+            break;
+        }
+    }
+
+    prl(used);
+}
+
+int main() {
+    test = 1;
+    // scl(test);
+    while (test--) solve();
+} 
