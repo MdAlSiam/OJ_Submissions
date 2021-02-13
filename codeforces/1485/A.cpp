ffa90cd880b667226ec7f86a0b09ef055ef7db09
MdAlSiam <md.al.siam.008@gmail.com>
2021-02-13 10:18:00 +0600

    Add solution for problem `A - Add and Divide` Link: https://codeforces.com/contest/1485/problem/A Tags: brute force, greedy, math, number theory, *1000 Ref: https://codeforces.com/contest/1485/submission/107252826

---
+++ codeforces/1485/A.cpp
@@ -0,0 +1,74 @@
+/// In the name of Allah SWT
+
+using namespace std;
+#include <bits/stdc++.h>
+
+#define ll long long int
+#define dd double
+
+#define scl(x) scanf("%lld", &x)
+#define scll(x, y) scanf("%lld %lld", &x, &y)
+#define scd(x) scanf("%lf", &x)
+#define scdd(x, y) scanf("%lf %lf", &x, &y)
+
+#define prl(x) printf("%lld\n", x)
+#define prll(x, y) printf("%lld %lld\n", x, y)
+#define prYes printf("YES\n")
+#define prNo printf("NO\n")
+#define aNewLine printf("\n")
+
+#define ON(n, i)            (n|(1LL<<i))
+#define OFF(n, i)           (n&(~(1LL<<i)))
+#define CHK(n, i)           (n&(1LL<<i))
+
+#define For(i, x, y)        for(ll i = x; i < y; i++)
+#define Mem(ara, x)         memset(ara, x, sizeof(ara))
+
+#define pb                  push_back
+#define pll                 pair<ll, ll >
+#define ff                  first
+#define ss                  second
+
+#define maxn                200005LL ///2x10^5 + 5
+
+#define pi                  acos(-1.00)
+#define eps                 0.0000000001 ///10^-10
+#define inf                 INT_MAX
+#define mod                 1000000007LL ///10^9+7
+
+ll t, test, temp;
+ll n, m, k, kount;
+ll a, b, c, ans, u, v;
+ll x, y, z = -1, maxi, mini;
+
+ll ara[maxn];
+string str;
+
+ll calcLog(ll num, ll base) {
+    if (base == 1) return inf;
+    ll log = 0;
+    while (num != 0) {
+        num /= base;
+        log++;
+    }
+    return log;
+}
+
+void solve() {
+    scll(a, b);
+    ans = inf;
+    For (i, 0, 33) {
+        ll ansHere = i;
+        ansHere += calcLog(a, b+i);
+        ans = min(ans, ansHere);
+    }
+    prl(ans);
+}
+
+int main() {
+    test = 1;
+    scl(test);
+    while (test--) solve();
+}
+
+// My feelings are not to be cared as well as cared
