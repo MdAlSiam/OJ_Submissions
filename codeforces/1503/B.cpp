b977cda7cb590028ccd3db1944bf2d6704c1d49e
MdAlSiam <md.al.siam.008@gmail.com>
2021-04-04 12:38:00 +0600

    Add solution for problem `B - 3-Coloring` Link: https://codeforces.com/contest/1503/problem/B Tags: constructive algorithms, games, interactive, *1700 Ref: https://codeforces.com/contest/1503/submission/111972505

--- codeforces/1503/B.cpp
+++ codeforces/1503/B.cpp
@@ -1,62 +1,110 @@
-#include <bits/stdc++.h>
-using namespace std;
+/// In the name of Allah SWT
 
-int main() {
-    int n;
-    cin >> n;
-    vector<pair<int, int> > evenSumCells, oddSumCells;
-    for (int i = 1; i <= n; i++) {
-        for (int j = 1; j <= n; j++) {
-            if ((i+j)%2) {
-                oddSumCells.push_back({i, j});
+using namespace std;
+#include <bits/stdc++.h>
+
+#define ll long long int
+#define dd double
+
+#define scl(x)              scanf("%lld", &x)
+#define scll(x, y)          scanf("%lld %lld", &x, &y)
+#define scd(x)              scanf("%lf", &x)
+#define scdd(x, y)          scanf("%lf %lf", &x, &y)
+
+#define prl(x)              printf("%lld\n", x)
+#define prll(x, y)          printf("%lld %lld\n", x, y)
+#define prYes               printf("YES\n")
+#define prNo                printf("NO\n")
+#define aNewLine            printf("\n")
+
+#define ON(n, i)            (n|(1LL<<i))
+#define OFF(n, i)           (n&(~(1LL<<i)))
+#define CHK(n, i)           (n&(1LL<<i))
+
+#define For(i, x, y)        for(ll i = x; i < y; i++)
+#define Mem(ara, x)         memset(ara, x, sizeof(ara))
+
+#define pb                  push_back
+#define pll                 pair<ll, ll >
+#define ff                  first
+#define ss                  second
+
+#define maxn                200005LL ///2x10^5 + 5
+
+#define pi                  acos(-1.00)
+#define eps                 0.0000000001 ///10^-10
+#define inf                 LONG_LONG_MAX
+#define mod                 1000000007LL ///10^9+7 
+
+ll t, test, temp;
+ll n, m, k, kount;
+ll c, ans, u, v;
+ll x, y, z = -1, maxi, mini;
+
+ll ara[maxn];
+string str;
+
+vector<pll > a;
+vector<pll > b;
+
+void solve() {
+    scl(n);
+    For (i, 0, n) {
+        For (j, 0, n) {
+            if ((i+j)%2 == 0) {
+                a.pb({i, j});
             }
             else {
-                evenSumCells.push_back({i, j});
+                b.pb({i, j});
             }
         }
     }
 
-    int x;
-
-    while (evenSumCells.size() and oddSumCells.size()) {
-        cin >> x;
+    while (a.size() and b.size()) {
+        scl(x);
         if (x == 1) {
-            cout << "2 " << oddSumCells.back().first << " " << oddSumCells.back().second << endl;
-            cout.flush();
-            oddSumCells.pop_back();
+            printf("2 %lld %lld\n", b.back().ff+1, b.back().ss+1);
+            fflush(stdout);
+            b.pop_back();
         }
         else {
-            cout << "1 " << evenSumCells.back().first << " " << evenSumCells.back().second << endl;
-            cout.flush();
-            evenSumCells.pop_back();
+            printf("1 %lld %lld\n", a.back().ff+1, a.back().ss+1);
+            fflush(stdout);
+            a.pop_back();
         }
     }
 
-    while (evenSumCells.size()) {
-        cin >> x;
+    while (a.size()) {
+        scl(x);
         if (x == 1) {
-            cout << "3 " << evenSumCells.back().first << " " << evenSumCells.back().second << endl;
-            cout.flush();
-            evenSumCells.pop_back();
+            printf("3 %lld %lld\n", a.back().ff+1, a.back().ss+1);
+            fflush(stdout);
+            a.pop_back();
         }
         else {
-            cout << "1 " << evenSumCells.back().first << " " << evenSumCells.back().second << endl;
-            cout.flush();
-            evenSumCells.pop_back();
+            printf("1 %lld %lld\n", a.back().ff+1, a.back().ss+1);
+            fflush(stdout);
+            a.pop_back();
         }
     }
 
-    while (oddSumCells.size()) {
-        cin >> x;
+    while (b.size()) {
+        scl(x);
         if (x == 2) {
-            cout << "3 " << oddSumCells.back().first << " " << oddSumCells.back().second << endl;
-            cout.flush();
-            oddSumCells.pop_back();
+            printf("3 %lld %lld\n", b.back().ff+1, b.back().ss+1);
+            fflush(stdout);
+            b.pop_back();
         }
         else {
-            cout << "2 " << oddSumCells.back().first << " " << oddSumCells.back().second << endl;
-            cout.flush();
-            oddSumCells.pop_back();
+            printf("2 %lld %lld\n", b.back().ff+1, b.back().ss+1);
+            fflush(stdout);
+            b.pop_back();
         }
     }
+} 
+
+int32_t main() {
+    test = 1;
+    // scl(test);
+    while (test--) solve();
 }
