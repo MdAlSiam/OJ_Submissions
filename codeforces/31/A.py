af907fcd2984adddd09e75e4b91b3caaf057b4b0
MdAlSiam <md.al.siam.008@gmail.com>
2021-05-27 23:47:00 +0600

    Add solution for problem `A - Worms Evolution` Link: https://codeforces.com/contest/31/problem/A Tags: implementation, *1200 Ref: https://codeforces.com/contest/31/submission/117525563

---
+++ codeforces/31/A.py
@@ -0,0 +1,16 @@
+n = int(input())
+ara = [int(i) for i in input().split(" ")]
+
+for i in range(n):
+    for j in range(i+1, n):
+        for k in range(j+1, n):
+            if (ara[i] == ara[j]+ara[k]):
+                print(i+1, j+1, k+1)
+                quit()
+            elif (ara[j] == ara[i]+ara[k]):
+                print(j+1, i+1, k+1)
+                quit()
+            elif (ara[k] == ara[i]+ara[j]):
+                print(k+1, i+1, j+1)
+                quit()
+print(-1)
