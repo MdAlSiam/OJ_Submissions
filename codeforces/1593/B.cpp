35337bbd93304d20820bc7a474a4198d553d2e78
MdAlSiam <md.al.siam.008@gmail.com>
2021-10-27 00:38:00 +0600

    Add solution for problem `B - Make it Divisible by 25` Link: https://codeforces.com/contest/1593/problem/B Tags: dfs and similar, dp, greedy, math, math, *900 Ref: https://codeforces.com/contest/1593/submission/133191918

---
+++ codeforces/1593/B.cpp
@@ -0,0 +1,72 @@
+/// In the name of Allah SWT
+
+using namespace std;
+#include <bits/stdc++.h>
+
+#define ll long long int
+#define dd double
+
+#define scl(x)              scanf("%lld", &x)
+#define scll(x, y)          scanf("%lld %lld", &x, &y)
+#define scd(x)              scanf("%lf", &x)
+#define scdd(x, y)          scanf("%lf %lf", &x, &y)
+#define db                  if (debug)
+
+#define prl(x)              printf("%lld\n", x)
+#define prll(x, y)          printf("%lld %lld\n", x, y)
+#define prYes               printf("YES\n")
+#define prNo                printf("NO\n")
+#define aNewLine            printf("\n")
+
+#define ON(n, i)            (n|(1LL<<i))
+#define OFF(n, i)           (n&(~(1LL<<i)))
+#define CHK(n, i)           (n&(1LL<<i))
+
+#define For(i, x, y)        for(ll i = x; i < y; i++)
+#define Mem(ara, x)         memset(ara, x, sizeof(ara))
+
+#define pb                  push_back
+#define pll                 pair<ll, ll >
+#define ff                  first
+#define ss                  second
+
+#define maxn                200005LL ///2x10^5 + 5
+
+#define pi                  acos(-1.00)
+#define eps                 0.0000000001 ///10^-10
+#define inf                 LONG_LONG_MAX
+#define mod                 1000000007LL ///10^9+7
+
+bool debug = false;
+
+ll t, test, temp;
+ll n, m, k, kount, kase;
+ll a, b, c, ans, u, v;
+ll x, y, z = -1, maxi, mini;
+
+ll ara[maxn];
+string str;
+
+void solve() {
+    cin >> str;
+
+    ans = inf;
+
+    For (i, 0, str.length()) {
+        For (j, i+1, str.length()) {
+            t = 10*(str[i]-'0')+(str[j]-'0');
+            if (t%25 == 0) ans = min(ans, (ll)str.length()-i-2LL);
+        }
+    }
+
+    prl(ans);
+} 
+ 
+int32_t main() {
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    test = 1;
+    kase = 0;
+    scl(test);
+    while (test--) solve();
+}
