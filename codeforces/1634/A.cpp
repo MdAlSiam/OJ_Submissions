f8a9f3682554f216ceae5771343f7dddebbe2973
MdAlSiam <md.al.siam.008@gmail.com>
2022-02-06 20:47:00 +0600

    Add solution for problem `A - Reverse and Concatenate` Link: https://codeforces.com/contest/1634/problem/A Tags: greedy, strings, *800 Ref: https://codeforces.com/contest/1634/submission/145404559

---
+++ codeforces/1634/A.cpp
@@ -0,0 +1,80 @@
+/// In the name of Allah SWT
+
+using namespace std;
+#include <bits/stdc++.h>
+
+#define ll long long int
+#define dd double
+
+#define scl(x)              scanf("%lld", &x)
+#define scll(x, y)          scanf("%lld %lld", &x, &y)
+#define scd(x)              scanf("%lf", &x)
+#define scdd(x, y)          scanf("%lf %lf", &x, &y)
+#define db                  if (debug)
+
+#define prl(x)              printf("%lld\n", x)
+#define prll(x, y)          printf("%lld %lld\n", x, y)
+#define prYes               printf("YES\n")
+#define prNo                printf("NO\n")
+#define aNewLine            printf("\n")
+
+#define ON(n, i)            (n|(1LL<<i))
+#define OFF(n, i)           (n&(~(1LL<<i)))
+#define CHK(n, i)           (n&(1LL<<i))
+
+#define For(i, x, y)        for(ll i = x; i < y; i++)
+#define Mem(ara, x)         memset(ara, x, sizeof(ara))
+
+#define pb                  push_back
+#define pll                 pair<ll, ll >
+#define ff                  first
+#define ss                  second
+
+#define pi                  acos(-1.00)
+#define eps                 0.0000000001 ///10^-10
+#define inf                 LONG_LONG_MAX
+#define mod                 1000000007LL ///10^9+7
+
+#define maxn                200005LL ///2x10^5 + 5
+
+bool debug = false;
+
+ll x, y, z = -1, maxi, mini;
+ll t, test, temp;
+ll n, m, k, kount, kase;
+ll a, b, c, ans, u, v;
+
+ll ara[maxn];
+string str;
+
+void solve() {
+    scll(n, k);
+    cin >> str;
+
+    bool palin = true;
+
+    ll lo = 0, hi = str.length()-1;
+
+    while (lo < hi) {
+        if (str[lo] != str[hi]) {
+            palin = false;
+            break;
+        }
+        lo++;
+        hi--;
+    }
+
+    if (palin or k == 0) printf("1");
+    else printf("2");
+
+    aNewLine;
+} 
+ 
+int32_t main() {
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    test = 1;
+    kase = 0;
+    scl(test);
+    while (test--) solve();
+}
