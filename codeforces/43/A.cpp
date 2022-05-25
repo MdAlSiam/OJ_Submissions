cde573a0feda48428ff2f82a84ae766e29300a58
MdAlSiam <md.al.siam.008@gmail.com>
2022-05-25 11:59:00 +0600

    Add solution for problem `A - Football` Link: https://codeforces.com/contest/43/problem/A Tags: strings, *1000 Ref: https://codeforces.com/contest/43/submission/158354381

--- codeforces/43/A.cpp
+++ codeforces/43/A.cpp
@@ -2,33 +2,29 @@
 using namespace std;
 
 int main() {
-    map<string, int> mp;
-    map<string, int>::iterator it;
-
     int n;
     cin >> n;
 
-    for (int i = 0; i < n; i++) {
+    string team1, team2;
+
+    int nGoals1 = 1, nGoals2 = 0;
+
+    cin >> team1;
+
+    for (int i = 1; i < n; i++) {
         string team;
         cin >> team;
 
-        mp[team]++;
-    }
-
-    string team1, team2;
-    int nGoals1 = 0, nGoals2 = 0;
-
-    for (it = mp.begin(); it != mp.end(); it++) {
-        if (it == mp.begin()) {
-            team1 = it->first;
-            nGoals1 = it->second;
+        if (team == team1) {
+            nGoals1++;
         }
         else {
-            team2 = it->first;
-            nGoals2 = it->second;
+            team2 = team;
+            nGoals2++;
         }
-    }    
+    }
 
-    nGoals1 > nGoals2 ? cout << team1 : cout << team2;
-    cout << endl;
+    // cout << nGoals1 << " YOYO " << nGoals2 << endl;
+
+    nGoals1 >= nGoals2 ? cout << team1 << endl : cout << team2 << endl;
 } 
